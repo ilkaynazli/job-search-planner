6ac5f3c82de1c33d564eea627468e54c99daf968
Ilkay Celik Gulsoy <ilkayncelik@gmail.com>
2019-01-19 05:16:40 +0000

    continued on data model

--- model.py
+++ model.py
@@ -52,6 +52,7 @@ class Company(db.Model):
                     name: {self.company_name},\
                     website: {self.company_website}>"
 
+
 class Application(db.Model):
     """Applications to the companies"""
 
@@ -69,7 +70,7 @@ class Application(db.Model):
                         db.ForeignKey('companies.company_id'),
                         nullable=False
                         )
-    date_applied = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    date_applied = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)    #check this one later!!!!
     position = db.Column(db.String(50), nullable=False)
     resume = db.Column(db.String(50), nullable=True)    #the location of the file 
     cover_letter = db.Column(db.String(50), nullable=True)  #the location of the file
@@ -82,6 +83,20 @@ class Application(db.Model):
 
     user = db.relationship('User')
     company = db.relationship('Company')
+    referer = db.relationship('Referer')
+
+
+    def __repr__(self):
+        """Human readable data"""
+        return f"<Application id: {self.application_id}, \
+                    company id: {self.company_id},\
+                    user id: {self.user_id}, \
+                    date applied: {self.date_applied},\
+                    position: {self.position},\
+                    resume: {self.resume},\
+                    cover letter: {self.cover_letter},\
+                    summary: {self.summary},\
+                    referer id: {self.referer_id}>"
 
 
 class Referer(db.Model):
@@ -89,13 +104,51 @@ class Referer(db.Model):
 
     __tablename__ = 'referers'
 
+    referer_id = db.Column(db.Integer, 
+                            primary_key=True,
+                            autoincrement=True,
+                            )
+    referer_name = db.Column(db.String(75), nullable=False)
+
+    application = db.relationship('Application')
+
+    def __repr__(self):
+        """Human readable data"""
+        return f"<Referer id: {self.referer_id}, \
+                    referer name: {self.referer_name}>"
 
 
-class ApplicationTopic(db.Model):
+class Interview(db.Model):
     """Topics asked in interview"""
 
+    __tablename__ = 'interviews'
+
+    interview_id = db.Column(db.Integer,
+                                primary_key=True,
+                                autoincrement=True,
+                                )
+    topic_id = db.Column(db.Integer,
+                        db.ForeignKey('topics.topic_id'),
+                        nullable=False
+                        )
+    application_id = db.Column(db.Integer,
+                        db.ForeignKey('applications.application_id'),
+                        nullable=False
+                        )
+    improvement = db.Column(db.Boolean, nullable=False)
 
 
+    application = db.relationship('Application')
+    topic =  db.relationship('Topic')
+
+    def __repr__(self):
+        """Human readable data"""
+        return f"<interview id: {self.interview_id},\
+                    topic id: {self.topic_id},\
+                    application id: {self.application_id},\
+                    improvement: {self.improvement}>"
+
+                    
 class Topic(db.Model):
     """Interview topics that could be asked"""
 
@@ -113,6 +166,7 @@ class Topic(db.Model):
         return f"<Topic id: {self.topic_id},\
                     topic: {self.topic}>"
 
+
 def connect_to_db(app, db_name):
     """Connect to database"""
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///' + db_name
@@ -120,7 +174,9 @@ def connect_to_db(app, db_name):
     db.app = app
     db.init_app(app)
 
+
 db_name = 'jobs'
+
 
 if __name__ == '__main__':
     """For running this interactively"""
